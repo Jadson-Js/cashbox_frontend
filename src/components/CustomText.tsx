e43706c7e558d7d8f4f20e3c0e9708c5c2ca92fe
jadson-js <jadson20051965@gmail.com>
2025-05-05 19:10:56 -0300

    add dynamic className

--- src/components/CustomText.tsx
+++ src/components/CustomText.tsx
@@ -1,6 +1,21 @@
+import React from "react";
 import { Text } from "react-native";
+import tw from "twrnc";
 import { ICustomText } from "../types/ICustomText";
 
-export default function CustomText({ content, size }: ICustomText) {
-  return <Text className="color-blue-700">{content}</Text>;
+export default function CustomText({
+  content,
+  size = "M",
+  className = "",
+}: ICustomText) {
+  const sizes = {
+    L: "text-4xl",
+    M: "text-2xl",
+    S: "text-xl",
+  };
+
+  // Combine size with any additional classes
+  const textClasses = `${sizes[size]} ${className}`.trim();
+
+  return <Text style={tw`${textClasses}`}>{content}</Text>;
 }
--- src/types/ICustomText.ts
+++ src/types/ICustomText.ts
@@ -1,5 +1,6 @@
 export interface ICustomText {
   content: string;
   size: "L" | "M" | "S";
+  className?: string;
   //color: "white" | "black";
 }
