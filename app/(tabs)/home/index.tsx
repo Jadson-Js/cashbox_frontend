6bd559e2990ffe26157d4e184aad4b80a0968613
jadson-js <jadson20051965@gmail.com>
2025-05-07 09:18:15 -0300

    add constants and update transactionList

--- app/(tabs)/home/index.tsx
+++ app/(tabs)/home/index.tsx
@@ -1,18 +1,19 @@
 import { CreditCard } from "@/src/components/dashboard/CreditCard";
 import { MonthPicker } from "@/src/components/dashboard/MonthPicker";
 import { TransactionList } from "@/src/components/dashboard/TransactionList";
+import { TransactionType } from "@/src/constants/enums";
 import { View } from "react-native";
 
-export default function HomeScreen() {
+export default function DashboardScreen() {
   return (
     <View className="h-full p-8 bg-white">
       <MonthPicker className="mb-8" />
 
       <CreditCard className="mb-12" />
 
-      <TransactionList className="mb-4" />
+      <TransactionList type={TransactionType.INCOME} className="mb-8" />
 
-      <TransactionList />
+      <TransactionList type={TransactionType.EXPENSE} />
     </View>
   );
 }
---
+++ src/constants/colors.ts
@@ -0,0 +1,5 @@
+export const colors = {
+  primary: "#007BB8",
+  red: "#C90C00",
+  green: "#40C900",
+};
