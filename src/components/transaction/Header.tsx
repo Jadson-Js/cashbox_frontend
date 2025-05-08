5a8a967dd9def5a5977b9056c94eaad81ce949ed
jadson-js <jadson20051965@gmail.com>
2025-05-08 17:09:00 -0300

    add onPress handler to buttons

--- src/components/transaction/Header.tsx
+++ src/components/transaction/Header.tsx
@@ -1,24 +1,40 @@
 import { IComponent } from "@/src/types/IComponent";
 import { MaterialIcons } from "@expo/vector-icons";
 import React from "react";
-import { View } from "react-native";
+import { TouchableOpacity, View } from "react-native";
 import { style } from "twrnc";
 
+import { useRouter } from "expo-router";
 import { CustomText } from "../ui/CustomText";
 
 export function Header({ className }: IComponent) {
-  const componentStyle = style(className);
-
+  const router = useRouter();
+  const componentStyle = style(
+    `flex flex-row justify-between items-center`,
+    className,
+  );
   const styleArrow = style("text-slate-400 w-8 h-8");
 
+  const handleExit = () => {
+    if (router.canGoBack()) {
+      router.back();
+    } else {
+      router.navigate("/(tabs)/home");
+    }
+  };
+
   return (
-    <View
-      style={componentStyle}
-      className="flex flex-row justify-between items-center"
-    >
-      <MaterialIcons name="close" size={30} style={styleArrow} />
-      <CustomText content="New Transaction" size="MB" />
-      <CustomText content="Save" size="XS" />
+    <View style={componentStyle}>
+      <TouchableOpacity onPress={handleExit}>
+        <MaterialIcons name="close" size={30} style={styleArrow} />
+      </TouchableOpacity>
+
+      <CustomText
+        content="New Transaction"
+        size="MB"
+        className="text-slate-600"
+      />
+      <CustomText content="Save" size="MB" />
     </View>
   );
 }
--- src/components/transaction/TypeSelector.tsx
+++ src/components/transaction/TypeSelector.tsx
@@ -2,35 +2,61 @@ import { colors } from "@/src/constants/colors";
 import { IComponent } from "@/src/types/IComponent";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import React from "react";
-import { View } from "react-native";
+import { TouchableOpacity, View } from "react-native";
 import { style } from "twrnc";
 import { shadow } from "../../constants/styles";
 import { CustomText } from "../ui/CustomText";
 
 export function TypeSelector({ className }: IComponent) {
+  const [isIncome, setIsIncome] = React.useState(true);
   const componentStyle = style(className);
-  const selector = style(`bg-[${colors.primary}]`);
 
-  const styleIcon = style("text-white w-8 h-8");
-  const x = style("text-slate-400 w-8 h-8");
+  const viewBase = style(
+    `flex-1 flex flex-row gap-2 rounded-full px-4 py-2 items-center justify-center `,
+  );
+  const viewSelected = style(`bg-[${colors.primary}]`);
+
+  const iconBase = style("w-8 h-8");
+
+  const iconSelected = style("text-white");
+  const iconNotSelected = style("text-slate-400");
 
   return (
     <View
       className="bg-white border rounded-full  border-slate-200 flex flex-row  items-center"
       style={[shadow, componentStyle]}
     >
-      <View
-        className="flex-1 flex flex-row gap-2 rounded-full px-4 py-2"
-        style={selector}
+      <TouchableOpacity
+        onPress={() => setIsIncome(true)}
+        style={[viewBase, isIncome && viewSelected]}
       >
-        <MaterialCommunityIcons name="cash-plus" size={30} style={styleIcon} />
-        <CustomText content="Income" size="M" className="text-white" />
-      </View>
+        <MaterialCommunityIcons
+          name="cash-plus"
+          size={30}
+          style={[iconBase, isIncome ? iconSelected : iconNotSelected]}
+        />
+        <CustomText
+          content="Income"
+          size="M"
+          className={isIncome ? "text-white" : ""}
+        />
+      </TouchableOpacity>
 
-      <View className="flex-1 flex flex-row gap-2 rounded-full px-4 py-2">
-        <MaterialCommunityIcons name="cash-plus" size={30} style={x} />
-        <CustomText content="Income" size="M" />
-      </View>
+      <TouchableOpacity
+        onPress={() => setIsIncome(false)}
+        style={[viewBase, !isIncome && viewSelected]}
+      >
+        <MaterialCommunityIcons
+          name="cash-minus"
+          size={30}
+          style={[iconBase, !isIncome ? iconSelected : iconNotSelected]}
+        />
+        <CustomText
+          content="Expenses"
+          size="M"
+          className={!isIncome ? "text-white" : ""}
+        />
+      </TouchableOpacity>
     </View>
   );
 }
