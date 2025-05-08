e0e889a160dc622f13aa42d7f408f75818bef210
jadson-js <jadson20051965@gmail.com>
2025-05-08 18:32:58 -0300

    add button[

--- app/(tabs)/home/TransactionScreen.tsx
+++ app/(tabs)/home/TransactionScreen.tsx
@@ -1,6 +1,7 @@
 import { Form } from "@/src/components/transaction/Form";
 import { Header } from "@/src/components/transaction/Header";
 import { TypeSelector } from "@/src/components/transaction/TypeSelector";
+import { CustomButton } from "@/src/components/ui/CustomButton";
 import { View } from "react-native";
 
 export default function TransactionScreen() {
@@ -12,6 +13,12 @@ export default function TransactionScreen() {
         <TypeSelector className="mb-16" />
 
         <Form />
+
+        <CustomButton
+          content="Create new"
+          onPress={() => console.log("save")}
+          className="absolute bottom-8 left-0 right-0 flex items-center mx-8"
+        />
       </View>
     </>
   );
