a9f376380e14ade68b3294acedba19cef3efaa4d
jadson-js <jadson20051965@gmail.com>
2025-05-09 11:36:17 -0300

    add icon selector

--- src/components/createCategory/DefineIcon.tsx
+++ src/components/createCategory/DefineIcon.tsx
@@ -1,20 +1,19 @@
-import { colors } from "@/src/constants/colors";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { View } from "react-native";
-import { style } from "twrnc";
 import { CustomText } from "../ui/CustomText";
 
 export function DefineIcon() {
-  const colorsView = Object.entries(colors).map(([name, hex]) => {
-    const styleComponent = style(`w-10 h-10 rounded-full bg-[${hex}]`);
+  const icons = ["shopping", "car", "food", "store"];
 
-    return <View style={styleComponent} key={name}></View>;
+  const iconsView = icons.map((name, index) => {
+    return <MaterialCommunityIcons key={index} name={name} size={50} />;
   });
 
   return (
     <View>
       <CustomText content="Icon" size="L" className="mb-4 text-slate-600" />
 
-      <View className="flex flex-row gap-4">{colorsView}</View>
+      <View className="flex flex-row gap-4">{iconsView}</View>
     </View>
   );
 }
