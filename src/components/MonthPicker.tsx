2c855a92b04ec7f0b0711bd318b532bd61e69cc7
jadson-js <jadson20051965@gmail.com>
2025-05-15 15:00:24 -0300

    add dynamic months

--- src/components/MonthPicker.tsx
+++ src/components/MonthPicker.tsx
@@ -2,14 +2,37 @@ import { IComponent } from "@/src/types/IComponent";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import React from "react";
 import { View } from "react-native";
-import { style } from "twrnc";
+import tw, { style } from "twrnc";
 import { shadow } from "../constants/styles";
 import { CustomText } from "./ui/CustomText";
 
 export function MonthPicker({ className }: IComponent) {
   const componentStyle = style(className);
+  const [months, setMonths] = React.useState<string[]>([
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ]);
+  const [currentMonth, setCurrentMonth] = React.useState<number>(2);
 
-  const styleArrow = style("text-slate-400 w-8 h-8");
+  const handleChangeMonth = (next: number) => {
+    if (
+      (next === -1 && currentMonth === 0) ||
+      (next === 1 && currentMonth === 11)
+    )
+      return;
+
+    setCurrentMonth((currentMonth + next) % 12);
+  };
 
   return (
     <View
@@ -19,13 +42,17 @@ export function MonthPicker({ className }: IComponent) {
       <MaterialCommunityIcons
         name="chevron-left"
         size={30}
-        style={styleArrow}
+        style={tw`text-slate-400 w-8 h-8`}
+        onPress={() => handleChangeMonth(-1)}
       />
-      <CustomText content="Janeiro" size="XS" />
+
+      <CustomText content={months[currentMonth]} size="XS" />
+
       <MaterialCommunityIcons
         name="chevron-right"
         size={30}
-        style={styleArrow}
+        style={tw`text-slate-400 w-8 h-8`}
+        onPress={() => handleChangeMonth(1)}
       />
     </View>
   );
